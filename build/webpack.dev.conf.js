320133e1a1d255e823dccd28a44a8bfb45dd0cf1
zhangjietao <zhangjietao@tuandai.com>
2018-10-09 11:22:51 +0800

    up

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -8,8 +8,7 @@ const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const portfinder = require('portfinder')
-
+const portfinder = require('portfinder') 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
@@ -52,10 +51,12 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
+    
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
-      inject: true
+      inject: true,
+      favicon:path.resolve('favicon.ico')   //新增
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
